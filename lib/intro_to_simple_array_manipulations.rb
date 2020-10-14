f3ed38a63c46912ca2f1ed56466a1df2b126b928
Alan sanchez <asanchezhaza@gmail.com>
2020-10-14 05:49:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,20 +6,21 @@ def using_push(colors_in_the_rainbow, next_color)
     
  end
  
+ 
  def using_unshift( bouroughs_in_nyc, new_neighborhood )
   
    bouroughs_in_nyc.unshift (new_neighborhood)
  end
+ 
  
  def using_pop (continents)
    
  other_continent =continents.pop
  
  
- 
-
-
- end
+  end
+  
+  
  
  
  
