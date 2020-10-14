7a3de630c75e5eb9676ed20c1edf2de73be25ae4
Alan sanchez <asanchezhaza@gmail.com>
2020-10-14 05:48:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,10 +6,9 @@ def using_push(colors_in_the_rainbow, next_color)
     
  end
  
- def using_unshift( bouroughs_in_nyc, new_neighborhood = "Staten Island")
-   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-   new_neighborhood = "Staten Island"
-   bouroughs_in_nyc.unshift ("Staten Island")
+ def using_unshift( bouroughs_in_nyc, new_neighborhood )
+  
+   bouroughs_in_nyc.unshift (new_neighborhood)
  end
  
  def using_pop (continents)
