b4a1ffdad2cf4839bbf256ec85ce689187a8a913
Alan sanchez <asanchezhaza@gmail.com>
2020-10-14 04:57:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,7 +15,7 @@ def using_push(colors_in_the_rainbow, next_color = "violet"  )
  def using_pop (continents)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
  other_continent = continents.pop 
- 
+ continents 
 
  end
  
