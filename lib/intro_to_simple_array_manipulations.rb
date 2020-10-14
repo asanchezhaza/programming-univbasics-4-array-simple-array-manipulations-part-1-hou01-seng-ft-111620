42d96626a1f88367c8601c28983078bed39ce7f5
Alan sanchez <asanchezhaza@gmail.com>
2020-10-14 04:47:29 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+ 
+def using_push(colors_in_the_rainbow, next_color = "violet"  )
+ colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    next_color = "violet"
+    colors_in_the_rainbow.push ("violet")
+    
+ end
+ 
+ def using_unshift( bouroughs_in_nyc, new_neighborhood = "Staten Island")
+   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+   new_neighborhood = "Staten Island"
+   bouroughs_in_nyc.unshift ("Staten Island")
+ end
+ 
+ def using_pop (continents)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+ continents.pop
+
+ end
+ 
+ 
+   
